7e7a72d06a478927f295653a00e278268613e3a1
jonathontran <103056909@student.swin.edu.au>
2023-08-13 12:11:56 +1000

    implemented some isolated methods as per functional requirements

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import mysql.connector
+import random
+import string
+import hashlib
 
 app = Flask(__name__)
 
@@ -12,12 +15,28 @@ def create_connection():
     )
     return connection
 
+def create_code():
+    characters = string.ascii_uppercase + string.digits
+    code = ''.join(random.choice(characters) for i in range(6))
+    return code
+
+def create_password(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation # All upper and lowercase letters, digits, and punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def encrypt(input):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
 @app.route('/<code>')
 def code(code):
+    #validate code to prevent SQL injection
     return f'{code}'
 
 if __name__ == '__main__':
