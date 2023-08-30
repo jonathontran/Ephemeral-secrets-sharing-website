df7cf88d2555578cf58c65114b2c49eb2857ef6a
Jesse Lyon <jesse.a.lyon+github@gmail.com>
2023-08-30 14:24:14 +1000

    Added code to ensure that the user cannot pick a past date in the datepicker

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -11,8 +11,15 @@ function fillDate () {
     // Converts date to a string which can be input into the HTML
     let oneWeek = date.getFullYear().toString().padStart(4, '0') + '-' + (date.getMonth()+1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
 
-    // Insert the date into the HTML
     document.getElementById('date').value = oneWeek;
+
+
+    // Set the min date for the date picker to today so that the user cannot select past dates.
+    var today = new Date();
+    let minDate = today.getFullYear().toString().padStart(4, '0') + '-' + (today.getMonth()+1).toString().padStart(2, '0') + '-' + today.getDate().toString().padStart(2, '0');
+
+    // Insert the date into the HTML
+    document.getElementById("date").min = minDate;    
 }
 
 window.onload = function() {
